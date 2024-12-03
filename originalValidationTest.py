291feab1d68f53af39ac52a0303c6d2165066fab
tjl9096 <tjl9096@rit.edu>
2024-12-03 00:33:09 -0500

    New base model

--- originalValidationTest.py
+++ originalValidationTest.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov5s.pt")
+model = YOLO("yolov5su.pt")
 
 # Customize validation settings
 validation_results = model.val(data="coco128.yaml", imgsz=640, batch=16, conf=0.25, iou=0.6, device="cpu")
