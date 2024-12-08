81c9bb4ebf1e848918cf35d9302dcd82f198da5f
tjl9096 <tjl9096@rit.edu>
2024-12-07 21:34:45 -0500

    moved grayscale image location

---
+++ compareGrayscale.py
@@ -0,0 +1,20 @@
+#################################################
+# Machine Learning
+# Final Group Poject
+# 
+# This file will compare the original versus the grayscale
+# version of the coco128 dataset
+#################################################
+
+from ultralytics import YOLO
+
+# load the models to train on
+base_model = YOLO("yolov5s.pt")
+grayscale_model = YOLO("yolov5s.pt")
+
+# train each of the models on the corresponding data
+# base_results = base_model.train(data="coco128.yaml", epochs=1, imgsz=640)
+grayscale_results = grayscale_model.train(data="grayscale.yaml", epochs=1, imgsz=640)
+
+print("\n\n\n\n\n")
+print(grayscale_results)
